6214bff4d7aac9d26c06e9e3c23e546db201173f
EndriSefa <75164997+EndriSefa@users.noreply.github.com>
2020-12-26 15:52:29 +0100

    Commit Zero
    Commit per importare lo Spring Initializr

---
+++ ProgrammaOW/src/main/java/com/univpm/ProgrammaOW/ProgrammaOwApplication.java
@@ -0,0 +1,13 @@
+package com.univpm.ProgrammaOW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammaOwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammaOwApplication.class, args);
+	}
+
+}
